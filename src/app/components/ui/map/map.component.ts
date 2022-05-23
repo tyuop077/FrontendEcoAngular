562288bac8d5fa3c7e2931a8d581d6741a9bd69f
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-05-23 03:55:52 +0300

    Added recycling points page & map markers

--- src/app/components/ui/map/map.component.ts
+++ src/app/components/ui/map/map.component.ts
@@ -16,9 +16,20 @@ export class MapComponent implements AfterViewInit {
 			zoom: 19
 		});
 
-		const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
 
-		tiles.addTo(this.map);
+		const icon = L.icon({
+			iconUrl: "assets/marker.svg"
+		})
+
+		L.marker([55.791306, 49.121525], {icon}).bindPopup(
+			`<b>ул.Кремлёвская, 88</b>
+                    <p>Пластик, стекло, бумага, металл, старая одежда, батареи, аккумуляторы...</p>`
+		).addTo(this.map);
+		L.marker([55.792129, 49.122045], {icon}).bindPopup(
+			`<b>ул.Кремлёвская, 35</b>
+                    <p>Стекло, бумага, металл, старая одежда, батареи</p>`
+		).addTo(this.map);
 	}
 
 	constructor() { }
