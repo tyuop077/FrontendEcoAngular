3a260f916d9facd25ed187ba561b344fa7ac63ca
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-06-03 21:03:24 +0300

    Added profile redirect if unauthorized

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Router } from '@angular/router';
+import { DialogService } from '@services/dialog.service';
+import { SignInModalComponent } from '@components/modals/sign-in-modal/sign-in-modal.component';
+import { storageService } from '@utils/local-storage';
 
 @Component({
 	selector: 'app-profile',
@@ -8,11 +12,16 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-	constructor() { }
+	constructor(private router: Router, private dialog: DialogService) { }
 
 	tab: number = 0;
 
 	ngOnInit(): void {
+		if (!storageService.getStorageValue("user_cache")) {
+			// not authorized
+			this.router.navigate([""]);
+			this.dialog.openDialog(SignInModalComponent);
+		}
 	}
 
 	setTab(index: number) {
